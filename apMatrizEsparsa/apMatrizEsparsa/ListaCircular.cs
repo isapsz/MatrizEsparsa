5d5dbb6ce929adde24aa3787fce822680bd58774
isapsz <42344186+isapsz@users.noreply.github.com>
2019-03-21 13:50:07 -0300

    a

--- apMatrizEsparsa/apMatrizEsparsa/ListaCircular.cs
+++ apMatrizEsparsa/apMatrizEsparsa/ListaCircular.cs
@@ -8,6 +8,45 @@ namespace apMatrizEsparsa
 {
     class ListaCircular
     {
-       
+        protected Celula cabeca, atual, anterior;
+        protected int qtd;
+        protected int linhas, colunas;
+
+        public ListaCircular(int linhas, int colunas)
+        {
+            cabeca = new Celula(0, -1, -1, null, null);
+            qtd = 0;
+
+            Celula p = cabeca;
+            for(int i =0; i< linhas; i++)
+            {
+                Celula linhaCabeca = new Celula(0, i, -1, null, null);
+
+                p.Abaixo = linhaCabeca;
+                p.Direita = p;
+                p = p.Abaixo;
+            }
+            p.Abaixo = cabeca;
+            p.Direita = p;
+        }
+
+        public void InserirElemento(double valor, int linha,  int coluna)
+        {
+            if (linha < 0 || coluna < 0)
+                throw new Exception("Local de inserção não disponível");
+
+            if (valor == 0)
+                throw new Exception("Valor de inserção inválido");
+
+            Celula linhaCabeca = cabeca;
+            Celula colunaCabeca = cabeca;
+
+            qtd++;
+        }
+
+        public void RemoverElemento()
+        {
+
+        }
     }
 }
--- apMatrizEsparsa/apMatrizEsparsa/NoLista.cs
+++ apMatrizEsparsa/apMatrizEsparsa/NoLista.cs
@@ -10,17 +10,14 @@ namespace apMatrizEsparsa
     {
         Dado info;
         NoLista<Dado> prox;
-        NoLista<Dado> ant;
 
         public Dado Info { get => info; set => info = value;  }
         internal NoLista<Dado> Prox { get => prox; set => prox = value; }
-        internal NoLista<Dado> Ant { get => ant; set => ant = value; }
 
         public NoLista(Dado info, NoLista<Dado> prox, NoLista<Dado> ant)
         {
             Info = info;
             Prox = prox;
-            Ant = ant;
         }
     }
 }
