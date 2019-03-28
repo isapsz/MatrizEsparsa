3cea2734ca6788c5a7aefe5fea10a687d5d7f102
isapsz <42344186+isapsz@users.noreply.github.com>
2019-03-28 09:16:50 -0300

    b

--- apMatrizEsparsa/apMatrizEsparsa/ListaCircular.cs
+++ apMatrizEsparsa/apMatrizEsparsa/ListaCircular.cs
@@ -9,9 +9,12 @@ namespace apMatrizEsparsa
 {
     class ListaCircular
     {
-        protected Celula cabeca;
+        protected Celula cabeca, esquerda, cima;
         protected int qtd;
-        protected int tamanhoLinhas, tamanhoColunas;
+        protected int tamanhoColunas, tamanhoLinhas;
+
+        public int TamanhoLinhas { get => tamanhoLinhas;}
+        public int TamanhoColunas { get => tamanhoColunas; }
 
         public ListaCircular(int l, int c)
         {
@@ -22,11 +25,14 @@ namespace apMatrizEsparsa
             this.tamanhoLinhas = l;
             this.tamanhoColunas = c;
 
+            this.cima = null;
+            this.esquerda = null;
+
             qtd = 0;
 
             Celula p = cabeca;
-            
-            for(int i =0; i< tamanhoLinhas; i++)
+
+            for (int i = 0; i < tamanhoLinhas; i++)
             {
                 Celula linhaCabeca = new Celula(0, i, -1);
 
@@ -51,7 +57,7 @@ namespace apMatrizEsparsa
         }
         protected Celula CriarCelula(double valor, int linha, int coluna)
         {
-            if (linha < 0 || coluna < 0 || linha > tamanhoLinhas-1 || coluna > tamanhoColunas-1)
+            if (linha < 0 || coluna < 0 || linha > tamanhoLinhas - 1 || coluna > tamanhoColunas - 1)
                 throw new Exception("Local de inserção não disponível");
 
             if (valor == 0)
@@ -62,28 +68,16 @@ namespace apMatrizEsparsa
 
         public void InserirElemento(double valor, int linha, int coluna)
         {
-            if (ValorDe(linha, coluna) == 0)
+            if (ValorDe(linha, coluna) != 0)
                 throw new Exception("Essa célula já existe");
 
-            Celula nova = CriarCelula(valor, linha, coluna), cima = cabeca, esquerda = cabeca;
-
-            while (esquerda.Linha < nova.Linha && esquerda.Abaixo != esquerda)
-                esquerda = esquerda.Abaixo;
-
-            while (esquerda.Direita.Coluna < nova.Coluna && esquerda.Direita != esquerda && esquerda.Direita.Coluna != -1)
-                esquerda = esquerda.Direita;
-
-            while (cima.Coluna < nova.Coluna && cima.Direita != cima)
-                cima = cima.Direita;
-
-            while(cima.Abaixo.Linha < nova.Linha  && cima.Abaixo != cima && cima.Abaixo.Linha != -1)
-                 cima = cima.Abaixo;
+            Celula nova = CriarCelula(valor, linha, coluna);
 
             nova.Abaixo = cima.Abaixo;
             nova.Direita = esquerda.Direita;
             esquerda.Direita = nova;
             cima.Abaixo = nova;
-            
+
             qtd++;
         }
 
@@ -92,13 +86,22 @@ namespace apMatrizEsparsa
             if (linha < 0 || coluna < 0 || linha > tamanhoLinhas || coluna > tamanhoColunas)
                 throw new Exception("Local procurado inválido!!!");
 
-            Celula procura = cabeca;
+            esquerda = cabeca;
+            cima = cabeca;
 
-            while (procura.Linha < linha && procura.Abaixo != procura)
-                procura = procura.Abaixo;
+            while (esquerda.Linha < linha && esquerda.Abaixo != esquerda)
+                esquerda = esquerda.Abaixo;
 
-            while (procura.Coluna < coluna && procura.Direita.Coluna != -1)
-                procura = procura.Direita;
+            while (esquerda.Direita.Coluna < coluna && esquerda.Direita != esquerda && esquerda.Direita.Coluna != -1)
+                esquerda = esquerda.Direita;
+
+            while (cima.Coluna < coluna && cima.Direita != cima)
+                cima = cima.Direita;
+
+            while (cima.Abaixo.Linha < linha && cima.Abaixo != cima && cima.Abaixo.Linha != -1)
+                cima = cima.Abaixo;
+
+            Celula procura = esquerda.Direita;
 
             if (procura.Coluna != coluna)
                 return 0;
@@ -108,11 +111,20 @@ namespace apMatrizEsparsa
 
         public void RemoverElemento(int linha, int coluna)
         {
-            if (ValorDe(linha, coluna) != 0)
+            if (ValorDe(linha, coluna) == 0)
                 throw new Exception("Essa célula não existe");
+            
+            Celula remover = esquerda.Direita;
 
+            cima.Abaixo = remover.Abaixo;
+            esquerda.Direita = remover.Direita;
+        }
 
-
+        public void Zerar()
+        {
+            cabeca = null;
+            tamanhoLinhas = 0;
+            tamanhoColunas = 0;
         }
 
         public void Exibir(DataGridView dgv)
@@ -124,6 +136,6 @@ namespace apMatrizEsparsa
             for (int i = 0; i < tamanhoLinhas; i++)
                 for (int x = 0; x < tamanhoColunas; x++)
                     dgv.Rows[i].Cells[x].Value = ValorDe(i, x);
-        }        
+        }
     }
 }
--- apMatrizEsparsa/apMatrizEsparsa/frmMatrizEsparsa.cs
+++ apMatrizEsparsa/apMatrizEsparsa/frmMatrizEsparsa.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,13 +22,46 @@ namespace apMatrizEsparsa
 
         private void frmMatrizEsparsa_Load(object sender, EventArgs e)
         {
+
+            LerArquivo(ref matriz);
             matriz = new ListaCircular(4, 3);
             matriz.InserirElemento(3, 2, 1);
             matriz.InserirElemento(4, 1, 1);
             matriz.InserirElemento(3, 3, 1);
             matriz.InserirElemento(15, 2, 0);
-            textBox2.Text = matriz.ValorDe(2,0).ToString();
+            textBox2.Text = matriz.ValorDe(2, 0).ToString();
             matriz.Exibir(dgvExibicao1);
+            matriz.RemoverElemento(1, 1);
+            matriz.RemoverElemento(2, 1);
+            matriz.Exibir(dgvExibicao1);
+        }
+
+        private void LerArquivo(ref ListaCircular lista)
+        {
+            if (dlgArquivo.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader arq = new StreamReader(dlgArquivo.FileName);
+                while (!arq.EndOfStream)
+                {
+                    string linha;
+                    lista = new ListaCircular(int.Parse(arq.ReadLine()), int.Parse(arq.ReadLine()));
+
+                    for (int l = 0; l < lista.TamanhoLinhas; l++)
+                    {
+                        linha = arq.ReadLine();
+                        string[] vetorValores = linha.Split(',');
+
+                        for (int c = 0; c < lista.TamanhoColunas; c++)
+                        {
+                            lista.InserirElemento(int.Parse(vetorValores[c]), 
+                                                  int.Parse(vetorValores[c + 1]), 
+                                                  int.Parse(vetorValores[c + 2]));
+                        }
+                    }
+                        
+                }
+            }
+
         }
     }
 }
